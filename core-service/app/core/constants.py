6a329190c53b209dacf4c59e4a770b3eac4e9300
yaten1983 <negi.yaten@gmail.com>
2026-04-24 17:13:04 +0530

    updated feature flags

--- core-service/app/core/constants.py
+++ core-service/app/core/constants.py
@@ -20,29 +20,29 @@ REVENUE_MODULE_ENABLED = "revenue_module_enabled "
 SOURCING_MODULE_ENABLED = "sourcing_module_enabled"
 
 # Banking module
-BOOK_ENABLED = "book_module_enabled"
+BOOK_MODULE_ENABLED = "book_module_enabled"
 BOOK_CHART_OF_ACCOUNT_ENABLED = "book_chart_of_account_enabled"
 
 # Tax module
-TAXANDCHARGES_ENABLED = "taxandcharges_module_enabled"
+TAXANDCHARGES_MODULE_ENABLED = "taxandcharges_module_enabled"
 
 # Subscription module
-SUBSCRIPTION_ENABLED = "subscriptions_module_enabled"
+SUBSCRIPTIONS_MODULE_ENABLED = "subscriptions_module_enabled"
 
 # Analystics module
-ANALYTICS_ENABLED = "analytics_module_enabled"
+ANALYTICS_MODULE_ENABLED = "analytics_module_enabled"
 
 # QSeal module
-QSEAL_ENABLED = "qseal_module_enabled"
+QSEAL_MODULE_ENABLED = "qseal_module_enabled"
 
 # User module
-USERS_ENBALED = "users_module_enabled"
+USERS_MODULE_ENABLED = "users_module_enabled"
 
 # Roles module
-ROLES_ENABLED = "roles_module_enabled"
+ROLES_MODULE_ENABLED = "roles_module_enabled"
 
 # Report module
-REPORTS_ENABLED = "reports_enabled"
+REPORTS_MODULE_ENABLED = "reports_module_enabled"
 
 # ── Error codes ─────────────────────────────────────────────────────
 FEATURE_DISABLED_CODE = "FEATURE_DISABLED"
